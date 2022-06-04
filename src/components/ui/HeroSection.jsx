17bd0b27798e185dd4afe5ebb578a791f19ae9c2
safa.jafarova <74055336+Safa002@users.noreply.github.com>
2022-06-04 13:09:31 +0400

    Update HeroSection.jsx

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -13,8 +13,8 @@ const HeroSection = () => {
           <Col lg="6" md="6">
             <div className="hero__content">
               <h2>
-                Discover rare digital art and collect 
-                <span> sell extraordinary</span> NFTs
+                Rəqəmsal sənəti kəşf edin və
+                <span> qeyri-adi NFT-ləri</span> satın
               </h2>
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -25,11 +25,11 @@ const HeroSection = () => {
               <div className="hero__btns d-flex align-items-center gap-4">
                 <button className=" explore__btn d-flex align-items-center gap-2">
                   <i class="ri-rocket-line"></i>{" "}
-                  <Link to="/market">Explore</Link>
+                  <Link to="/market">Kəşf et</Link>
                 </button>
                 <button className=" create__btn d-flex align-items-center gap-2">
                   <i class="ri-ball-pen-line"></i>
-                  <Link to="/create">Create</Link>
+                  <Link to="/create">Yarat</Link>
                 </button>
               </div>
             </div>
