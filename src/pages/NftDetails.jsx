a3612fd0ea630ac0cbc70933c40767b97c2d26ae
safa.jafarova <74055336+Safa002@users.noreply.github.com>
2022-06-04 13:11:25 +0400

    Update NftDetails.jsx

--- src/pages/NftDetails.jsx
+++ src/pages/NftDetails.jsx
@@ -61,7 +61,7 @@ const NftDetails = () => {
                   </div>
 
                   <div className="creator__detail">
-                    <p>Created By</p>
+                    <p>Müəllif</p>
                     <h6>{singleNft.creator}</h6>
                   </div>
                 </div>
@@ -69,7 +69,7 @@ const NftDetails = () => {
                 <p className="my-4">{singleNft.desc}</p>
                 <button className="singleNft-btn d-flex align-items-center gap-2 w-100">
                   <i class="ri-shopping-bag-line"></i>
-                  <Link to="/wallet">Place a Bid</Link>
+                  <Link to="/wallet">Təklif et</Link>
                 </button>
               </div>
             </Col>
